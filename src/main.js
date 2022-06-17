84da43064e463f9ebd7b2eff082dc27f28270d3c
Gruodis <43164261+Gruodis@users.noreply.github.com>
2022-06-17 08:45:09 +0300

    Update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Tailwind CSS!</h1>
+  <h1>Hello, Vite & Tailwind CSS!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
