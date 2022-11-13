df7cd5106a3e28f1aae40965b222bd557c3dada0
Aurelijus <ditnet@gmail.com>
2022-11-13 14:40:39 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,13 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
+
 export default {
+    plugins: [
+        handlebars({
+            partialDirectory: resolve(__dirname, './src/partials'),
+        }),
+    ],
     root: 'src',
     build: {
         outDir: '../dist',
