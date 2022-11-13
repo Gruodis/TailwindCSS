5ed5e1ec6e5ae9dd7c2a91f0631d5150e7b9fa7d
Aurelijus <ditnet@gmail.com>
2022-11-13 16:21:10 +0200

    Handlebars added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,48 @@
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
+
 export default {
     root: 'src',
     build: {
         outDir: '../dist',
+        assetsDir: 'assets',
+        emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                entryFileNames: 'assets/js/[name]-[hash].js',
+
+                assetFileNames: ({name}) => {
+                    if (/\.(gif|jpe?g|png)$/.test(name ?? '')){
+                        return 'assets/images/[name]-[hash][extname]';
+                    }
+
+                    if (/\.(svg)$/.test(name ?? '')){
+                        return 'assets/svg/[name]-[hash][extname]';
+                    }
+
+                    if (/\.css$/.test(name ?? '')) {
+                        return 'assets/css/[name]-[hash][extname]';
+                    }
+
+                    // default value
+                    // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+                    return 'assets/[name]-[hash][extname]';
+                },
+            },
+        }
     },
     server: {
         open: true
-    }
+    },
+    plugins: [
+        handlebars({
+            partialDirectory: resolve(__dirname, './src/partials'),
+            reloadOnPartialChange: true,
+
+            context: {
+                title: 'Hello, world!',
+            },
+        }),
+    ],
 }
